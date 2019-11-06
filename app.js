b66f9061d761788a8bb3dd3baee14bb8f186b6bc
Ghaidan Alhubaishi <alghaidan@yahoo.com>
2019-11-05 17:59:13 -0800

    edited the map

--- app.js
+++ app.js
@@ -66,17 +66,6 @@ function showSlides() {
     setTimeout(showSlides, 4000);
 }
 
-//location
-function initMap() {
-    // The location of Uluru
-    var uluru = { lat: 37.596421, lng: -122.367545 };
-    // The map, centered at Uluru
-    var map = new google.maps.Map(
-        document.getElementById('map'), { zoom: 4, center: uluru });
-    // The marker, positioned at Uluru
-    var marker = new google.maps.Marker({ position: uluru, map: map });
-}
-
 // menu tags
 function openPage(pageName, elmnt) {
     var i, tabcontent, tablinks;
